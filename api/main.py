885f3477d8e28f1c4945400315d789cce82d6c77
Jesusiacent <jesus.manuel.2@train.ia.center>
2023-08-01 23:32:14 -0600

    Clase 11

--- api/main.py
+++ api/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+#async def raiz():
+    #return{'Message': 'Hello World'}
 def raiz():
     return{'Hola': 'world'} # para correrlo abrir terminal y colocamos: cd api y damos enter. em la siguinete colocamos 'uvicorn (nombre de archivo):app --reload
 
@@ -11,3 +13,9 @@ def raiz():
 @app.get('/items/{item_id}')
 def read_item(item_id: int, m: str = None):
     return {'item_id': item_id, 'm': m}
+
+#@app.post('/items/')
+#async def insertar_libro(libro: Libro):
+    #return {'Massege': f'Libro: {libro.titulo} isertado correctamente'}
+
+    #inst thunder client
